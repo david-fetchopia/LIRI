41f4c530d7f83b30ba49c84a83bae2a4c5ab949b
DMa <davidweibinma@Weibins-MacBook-Pro-2.local>
2019-03-26 00:31:22 -0400

    movie-this function complete

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 let Spotify = require('node-spotify-api');
-let keys = require("./keys.js");
+let axios = require("axios");
 
+let keys = require("./keys.js");
 let input = process.argv;
 
 function liri_spotify(track_name) {
@@ -39,6 +40,54 @@ function liri_spotify(track_name) {
     
 }
 
+function liri_concert(artist_name) {
+    
+}
+
+function liri_movie(movie_name) {
+    if (movie_name === undefined) {
+        let queryUrl = "http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&apikey=trilogy";
+        axios.get(queryUrl).then(function(response){
+            console.log("* Title: " + response.data.Title);
+            console.log("* Year Released: " + response.data.Year);
+            console.log("* IMDB Rating: " + response.data.imdbRating);
+            console.log("* Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("* Country Produced: " + response.data.Country);
+            console.log("* Movie Language: " + response.data.Language);
+            console.log("* Movie Plot: " + response.data.Plot);
+            console.log("* Actors: " + response.data.Actors);
+        });
+    }
+    else {
+        movie_name = movie_name.split(" ");
+        let movie_name_query = "";
+        for(var i = 0; i < movie_name.length; i++) {
+            if (movie_name_query === "") {
+                movie_name_query += movie_name[i];
+            }
+            else {
+                movie_name_query += "+" + movie_name[i];
+            }
+            
+        }
+        let queryUrl = "http://www.omdbapi.com/?t=" + movie_name_query + "&y=&plot=short&apikey=trilogy";
+        axios.get(queryUrl).then(function(response){
+            console.log("* Title: " + response.data.Title);
+            console.log("* Year Released: " + response.data.Year);
+            console.log("* IMDB Rating: " + response.data.imdbRating);
+            console.log("* Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("* Country Produced: " + response.data.Country);
+            console.log("* Movie Language: " + response.data.Language);
+            console.log("* Movie Plot: " + response.data.Plot);
+            console.log("* Actors: " + response.data.Actors);
+        });
+    }
+    
+}
+
+function liri_dotxt() {
+
+}
 
 
 
@@ -46,6 +95,15 @@ function liri_spotify(track_name) {
 switch(input[2]) {
     case 'spotify-this-song':
         liri_spotify(input[3]);
+        break;
+    case 'do-what-it-says':
+        liri_dotxt;
+        break;
+    case 'concert-this':
+        liri_concert(input[3]);
+        break;
+    case 'movie-this':
+        liri_movie(input[3]);
         break;
     default:
         console.log('default case error');
